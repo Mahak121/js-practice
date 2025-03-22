5bbafaf0d57cde8dfe3a36b94e0bf2a5c01cdc5a
Mahak pal <152528499+Mahak121@users.noreply.github.com>
2025-03-22 10:26:18 +0000

    datatypes

---
+++ .devcontainer/01Basics/datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // Treat all javascript code as newer version
+
+/*
+Types of data type 
+number=>2 to power 53
+bigInt
+String=>"", ''
+boolean=> True/False
+null=> Standloan value
+undefined=> variable that has not been assigned a value
+symbol=> defined unique
+
+object
+*/
+console.log(typeof undefined) // undefined 
+console.log(typeof null) // object
